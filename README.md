2151ff8f5f3e64895faf131cef2a1b4174e8bd02
kailana-so <kailana.work@gmail.com>
2021-07-19 12:51:10 +1000

    loop break at end of round, score conditions

---
+++ README.md
@@ -0,0 +1,13 @@
+* A simple game of Zilch
+
+** version 1
+- Game play testing in node using prompt sync for play interaction
+- While loop for player round
+- Roll set randomised set of 6 numerals (dice)
+- Roll length adjusted each time a dice is taken as a score
+- Checking for valid choices
+- Score tracking and total when turn ends
+
+
+** version 2
+- Adding play turns and turn total tracking
--- zilchV1.js
+++ zilchV1.js
@@ -9,15 +9,11 @@
 const prompt = require('prompt-sync')();
 
 
-function rollChoice(){
+function playerRoll(){
 
     const values = {
             '1': 100,
-            '2': 0,
-            '3': 0,
-            '4': 0,
             '5': 50,
-            '6': 0,
             '111': 1000,
             '222': 200, 
             '333': 300,
@@ -26,29 +22,47 @@ function rollChoice(){
         }
 
     
-    let playerScore = [0]
+    let playerScore = []
     let sit = []
     let rollLength = 6  
     let diceTaken = [0]
     
-    while(sit.includes('sit') == false && rollLength > diceTaken.length - 1){
-        console.log(playerScore.includes('sit'))
+    while(!sit.includes('sit') && rollLength > diceTaken.length - 1){
+
         let roll = [...Array(rollLength - diceTaken.reduce((acc, num) => acc + num))].map(() => Math.floor(Math.random() * 6) + 1)
         console.log(roll)
 
+        // if roll length ie. dice left == ''(- empty) break out of loop
+        if(roll == '') break;
+        
+        // if roll doesnt contain 1 or 5 or 3 of a kind
+        // if(!roll.includes('1')) break;
+
+        // prompt player for choice
         const playerChoice = prompt('Choose your dice: ')
 
         // if str length != 3 split and search each value : search for the set of 3 in lookup
 
-        values[playerChoice] ? playerScore.push(values[playerChoice]) && diceTaken.push(playerChoice.split('').length) : sit.push(playerChoice)
-        console.log(diceTaken)
-        
-        
-        console.log(`You have choosen ${playerChoice}, your current score is ${playerScore}`)
+
+
+        // if the player choice is a valid score, push it to player score and split the length to add to dice taken array
+        // else check is the player choice is 'sit' and add to sit arr
+        // else score not valid
+        values[playerChoice] 
+        ? playerScore.push(values[playerChoice]) && diceTaken.push(playerChoice.split('').length) &&         console.log(`You have choosen ${playerChoice}, your current score is ${playerScore}`)
+        : playerChoice === 'sit' 
+            ? sit.push(playerChoice) && console.log(`You have choosen to ${playerChoice}, your current score is ${playerScore}`)
+            : console.log(`that's not valid!`)
+
+
+        // tracking dice
+        // console.log(diceTaken)
 
 
     }
 
-    return playerScore.reduce((acc, score) => acc + score)
+    return playerScore.length != 0 
+    ? `Your score is ${playerScore.reduce((acc, score) => acc + score)}` 
+    : 'You Zilched!'
 }
-console.log(rollChoice())
+console.log(playerRoll())
